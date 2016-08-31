ac457f36b70848e5eb8e6c087893ee9b16ce59f0
sainthkh <sainthkh@naver.com>
2016-08-31 10:44:47 +0900

    Initial Update.

---
+++ app/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+
+var http = require('http');
+var path = require('path');
+
+const exphbs = require('express-handlebars')
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+
+app.engine('.hbs', exphbs({
+  defaultLayout: 'layout',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname),
+  partialsDir: path.join(__dirname)
+}))
+
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname))
+
+module.exports = app
