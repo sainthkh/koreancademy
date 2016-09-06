60e41bbd52cd975637e2d9ea15084194bccb263c
sainthkh <sainthkh@naver.com>
2016-09-06 11:50:22 +0900

    Setup Auth. Most of the code are imported from Node Hero tutorial.

--- app/index.js
+++ app/index.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars')
+const session = require('express-session')
+const RedisStore = require('connect-redis')(session)
+
+const config = require('../config')
 const app = express();
 
 app.use(express.static('assets'))
 
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
+
+require('./auth').init(app)
+
+app.use(session({
+  store: new RedisStore({
+    url: config.redisStore.url
+  }),
+  secret: config.redisStore.secret,
+  resave: false,
+  saveUninitialized: false
+}))
 
 app.engine('.hbs', exphbs({
   defaultLayout: 'layout',
---
+++ config/index.js
@@ -0,0 +1,8 @@
+const config = {}
+
+config.redisStore = {
+  url: '//localhost:6379',
+  secret: 'test value'
+}
+
+module.exports = config
