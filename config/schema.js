7706d8a868f2b9d4c0612869a3ae7ecde4c17485
sainthkh <sainthkh@naver.com>
2016-09-09 17:31:12 +0900

    Added database schema.

---
+++ config/schema.js
@@ -0,0 +1,15 @@
+const sqlite3 = require("sqlite3").verbose();
+const path = require('path')
+const db = new sqlite3.Database(path.join(__dirname, "../assets/data.db"));
+
+db.serialize(function() {
+	db.run("create table users("+
+		"id int primary_key," +
+		"email varchar(255)," +
+		"password_has char(32)," +
+		"nickname varchar(30)," +
+		"access_level varchar(16)" +
+		")")
+
+	db.close()
+})
