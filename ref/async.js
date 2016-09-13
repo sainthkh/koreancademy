2a2e2e13eaeefea2d29a1a41d62119c2fb1ae167
sainthkh <sainthkh@naver.com>
2016-09-13 18:07:00 +0900

    Added async.js reference file.

---
+++ ref/async.js
@@ -0,0 +1,126 @@
+//
+// parallel
+////////////////////////////
+
+async.parallel([
+    function(callback) {
+        setTimeout(function() {
+            callback(null, 'one'); // err, result
+        }, 200);
+    },
+    function(callback) {
+        setTimeout(function() {
+            callback(null, 'two');
+        }, 100);
+    }
+],
+// optional callback
+function(err, results) {
+    // the results array will equal ['one','two'] even though
+    // the second function had a shorter timeout.
+});
+
+// an example using an object instead of an array
+async.parallel({
+    one: function(callback) {
+        setTimeout(function() {
+            callback(null, 1);
+        }, 200);
+    },
+    two: function(callback) {
+        setTimeout(function() {
+            callback(null, 2);
+        }, 100);
+    }
+}, function(err, results) {
+    // results is now equals to: {one: 1, two: 2}
+});
+
+//
+// series
+////////////////////////////
+
+async.series([
+    function(callback) {
+        // do some stuff ...
+        callback(null, 'one');
+    },
+    function(callback) {
+        // do some more stuff ...
+        callback(null, 'two');
+    }
+],
+// optional callback
+function(err, results) {
+    // results is now equal to ['one', 'two']
+});
+
+async.series({
+    one: function(callback) {
+        setTimeout(function() {
+            callback(null, 1);
+        }, 200);
+    },
+    two: function(callback){
+        setTimeout(function() {
+            callback(null, 2);
+        }, 100);
+    }
+}, function(err, results) {
+    // results is now equal to: {one: 1, two: 2}
+});
+
+//
+// waterfall
+////////////////////////////
+
+async.waterfall([
+    function(callback) {
+        callback(null, 'one', 'two');
+    },
+    function(arg1, arg2, callback) {
+        // arg1 now equals 'one' and arg2 now equals 'two'
+        callback(null, 'three');
+		// Got some error ? Don't wanna go further.
+      	// Provide true in callback and execution will stop.
+      	//callback(true,"Some error");
+    },
+    function(arg1, callback) {
+        // arg1 now equals 'three'
+        callback(null, 'done');
+    }
+], function (err, result) {
+    // result now equals 'done'
+});
+
+// Or, with named functions:
+async.waterfall([
+    myFirstFunction,
+    mySecondFunction,
+    myLastFunction,
+], function (err, result) {
+    // result now equals 'done'
+});
+function myFirstFunction(callback) {
+    callback(null, 'one', 'two');
+}
+function mySecondFunction(arg1, arg2, callback) {
+    // arg1 now equals 'one' and arg2 now equals 'two'
+    callback(null, 'three');
+}
+function myLastFunction(arg1, callback) {
+    // arg1 now equals 'three'
+    callback(null, 'done');
+}
+
+//
+// each
+/////////////////////
+
+async.each(emails,function(singleEmail,callback) {
+  // Emailer code
+  // singleEmail will be one value at a time.
+},function(err,data) {
+  // Once all done, comes here.
+});
+
