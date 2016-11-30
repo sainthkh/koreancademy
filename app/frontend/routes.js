91b23296065dc954e4f0ae98cdd7ec410bb92def
sainthkh <sainthkh@naver.com>
2016-11-30 20:37:37 +0900

    Added basic rendering main page.

--- app/frontend/routes.js
+++ app/frontend/routes.js
@@ -2,10 +2,13 @@
 
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
+import Layout from './components/Layout'
+import IndexPage from './components/IndexPage'
 
 const routes = (
-  <Route path="/">
-  </Route>
+	<Route path="/" component={Layout}>
+		<IndexRoute component={IndexPage}/>
+	</Route>
 );
 
 export default routes;
